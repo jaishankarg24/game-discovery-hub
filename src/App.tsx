a614d2d922fd74cb1fef47f860854d9419744476
jaishankar <jaishankarg24@gmail.com>
2023-05-06 12:20:43 +0530

    Building the Nav Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -12,8 +13,8 @@ function App() {
         lg: "250px 1fr",
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
